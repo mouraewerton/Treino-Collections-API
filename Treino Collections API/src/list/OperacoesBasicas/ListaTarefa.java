05d48bd4538a7a417783fc907f5d4550191c335c
Ewerton Moura <102037361+mouraewerton@users.noreply.github.com>
2023-10-18 13:36:16 -0300

    List corrigido e funcionando

--- Treino Collections API/src/list/OperacoesBasicas/ListaTarefa.java
+++ Treino Collections API/src/list/OperacoesBasicas/ListaTarefa.java
@@ -8,26 +8,59 @@ public class ListaTarefa {
     private List<Tarefa> tarefaList;
 
     public ListaTarefa() {
-        this.tarefaList = tarefaList;
+
+        this.tarefaList = new ArrayList<>();
     }
 
     public void adicionarTarefa (String descricao) {
+
         tarefaList.add(new Tarefa(descricao));
     }
 
     public void removerTarefa (String descricao) {
         List<Tarefa> tarefasParaRemover = new ArrayList<>();
-        for (Tarefa t : tarefaList) {
-            if (t.getDescricao().equalsIgnoreCase(descricao)) {
-                tarefasParaRemover.add(t);
+        if (!tarefaList.isEmpty()) {
+            for (Tarefa t : tarefaList) {
+                if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                    tarefasParaRemover.add(t);
+                }
             }
+            tarefaList.removeAll(tarefasParaRemover);
+        } else {
+            System.out.println("A lista está vazia !");
         }
-        tarefaList.removeAll(tarefasParaRemover);
     }
     public int obterNumeroTotalTarefas() {
         return tarefaList.size();
     }
     public void obterDescricoesTarefas() {
-        System.out.println(tarefaList);
+        if (!tarefaList.isEmpty()) {
+            System.out.println(tarefaList);
+        } else {
+            System.out.println("A lista está vazia !");
+        }
+    }
+    public static void main(String[] args) {
+        // Criando uma instância da classe ListaTarefa
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        listaTarefa.adicionarTarefa("Estudar Java Básico");
+        listaTarefa.adicionarTarefa("Trabalhar");
+        listaTarefa.adicionarTarefa("Treinar");
+
+        // Exibir numero total de tarefas na lista
+       System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+       // Exibir descriçoes das tarefas na lista
+        listaTarefa.obterDescricoesTarefas();
+
+        // Removendo tarefa da lista
+        listaTarefa.removerTarefa("Trabalhar");
+
+        // Exibir numero total de tarefas na lista após remoção
+        System.out.println("O atual numero de elementos na lista é " + listaTarefa.obterNumeroTotalTarefas());
+
+        // Exibir descrições atualizadas da lista
+        listaTarefa.obterDescricoesTarefas();
     }
 }
--- Treino Collections API/src/list/OperacoesBasicas/Tarefa.java
+++ Treino Collections API/src/list/OperacoesBasicas/Tarefa.java
@@ -5,10 +5,17 @@ public class Tarefa {
     private String descricao;
 
     public Tarefa (String descricao) {
+
         this.descricao = descricao;
     }
 
     public String getDescricao() {
+
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
         return descricao;
     }
 }
